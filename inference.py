7959dccfd97e79de491a2f078c4e3dc9328dcd2a
Warlord-K <warlordk28@gmail.com>
2023-10-17 20:27:10 +0530

    Fixed Loading Bug

--- inference.py
+++ inference.py
@@ -1,6 +1,5 @@
 import torch
 from diffusers import StableDiffusionXLPipeline
-from diffusers import UniPCScheduler
 
 path = 'segmind/SDXL-Mini' #model card
 prompt = "hyperrealistic glamour portrait of an old weary wizard surrounded by elemental magic, arcane, freckles, skin pores, pores, velus hair, macro, extreme details, looking at viewer"
@@ -13,7 +12,6 @@ with torch.inference_mode():
       path, torch_dtype=torch.float16, use_safetensors=True
     )
     pipe.to(f"cuda")
-    pipe.scheduler = UniPCScheduler.from_config(pipe.scheduler.config)
     pipe.unet.to(device=f"cuda", dtype=torch.float16, memory_format=torch.channels_last)
-    img = pipe(prompt=prompt,negative_prompt=negative_prompt, num_inference_steps=50, guidance_scale = 9, num_images_per_prompt=1,generator=gen).images[0]
+    img = pipe(prompt=prompt,negative_prompt=negative_prompt, num_inference_steps=50, guidance_scale = 9, num_images_per_prompt=1).images[0]
     img.save(f"image.png")
